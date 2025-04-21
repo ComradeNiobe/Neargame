b7124aaadf2f9298f0b1e550740b391f808f6c88
Comrade Niobe <126028983+ComradeNiobe@users.noreply.github.com>
2025-04-21 12:05:22 -0400

    Use LF line endings, hopefully please git actions

--- scripts/backup_saves.sh
+++ scripts/backup_saves.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script requires a git repository initialized
-# in data/ with an upstream remote configured. 
+# in data/ with an upstream remote configured.
 TIMESTAMP=$( date "+%H_%d_%m_%Y" )
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../data
